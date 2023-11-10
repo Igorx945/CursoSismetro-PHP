0d16413cac6d4a84920e5b5968f7230edc5eb1db
Igorx945 <128699017+Igorx945@users.noreply.github.com>
2023-11-10 14:37:47 -0300

    update

--- aula-021/fibonacci.php
+++ aula-021/fibonacci.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <?php
-function fibonacci($n1)
+function numero_primo($posicao)
 {
-    $sequencia = array();
-    $sequencia[1] = 2;
-    $sequencia[2] = 2;
+    $primos = array(2);
 
-    for ($i = 3; $i < $n1; $i++) {
-        $sequencia[$i] = $sequencia[$i - 1] + $sequencia[$i - 2];
+    $n = 3;
+    while (count($primos) < $posicao) {
+        $e_primo = true;
+        foreach ($primos as $primo) {
+            if ($n % $primo == 0) {
+                $e_primo = false;
+            }
+        }
+        if ($e_primo) {
+            $primos[] = $n;
+        }
+        $n++;
     }
-    return $sequencia[$n1 - 1];
+    return $primos[$posicao - 1];
 }
+
 ?>
 
 <html lang="pt-br">
@@ -22,7 +31,7 @@ function fibonacci($n1)
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         body {
-            background: linear-gradient(90deg, rgba(135, 228, 247, 1) 0%, rgba(9, 107, 121, 1) 53%, rgba(83, 78, 176, 1) 100%);
+            background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(255,0,52,1) 100%);
             height: 100vh;
             color: white;
             text-align: center;
@@ -32,66 +41,78 @@ function fibonacci($n1)
         }
 
         .formulario {
-            width: 100%;
+            width: 50%;
             margin: auto;
             padding: 25px;
-            border: solid 1px;
+            height: 100vh;
+            border-radius: 30px;
+            box-shadow: 5px 4px 20px rgb(78, 78, 78);
+            color: black;
+
 
         }
 
         .conteiner {
             border-radius: 25px;
             box-shadow: 3px 2px 10px rgb(78, 78, 78);
-            color: white;
-            background-color: #00d4ff;
+            color: black;
+            background-color: #ff0034;
+
         }
 
         .resultado {
             height: 5vh;
             color: white;
-            text-align: center;
-            align-items: center;
-            justify-content: center;
             border-radius: 25px;
             box-shadow: 3px 2px 10px rgb(78, 78, 78);
-            background-color: #00d4ff;
+            background-color: #ff0034;
         }
-        .form-control{
-            align-items: center;
-            justify-content: center;
-            text-align: center;
+
+        
+        img {
+            max-width: 50%;
+            max-height: 50%;
+            margin: auto;
+        }
+        h1, h2{
+            display: flex;
+            text-align: start;
 
         }
+
     </style>
 </head>
 
 <body class="formulario">
-    <h1>Calculadora Fibonacci</h1>
-    <form method="post">
-        <div class="conteiner">
-            <div class="mb-3">
-                <label class="form-label">Posicao</label>
-                <input type="text" name="posicao" class="form-control" placeholder="posicao da sequencia de fibonacci" value="<?php if (isset($_POST["posicao"])) echo $_POST["posicao"]; ?>">
+    <div class="form">
+        <h1>Calculadora de Numeros Primos</h1>
+        <img src="primos.jpg" alt="">
+        <form method="post">
+            <div class="conteiner">
+                <div class="mb-3">
+                    <h1>Posição:</h1>
+                    <input type="text" name="posicao" class="form-control" placeholder="posicao da sequencia de fibonacci" value="<?php if (isset($_POST["posicao"])) echo $_POST["posicao"]; ?>">
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-success">Enviar</button>
+                </div>
             </div>
-            <div class="mb-3">
-                <button type="submit" class="btn btn-success">Enviar</button>
-            </div>
-        </div>
-        <h2>Resultado:</h2>
-        <div class="resultado">
-            <?php
-            if (isset($_POST["posicao"])) {
-            ?>
-         
-                    <h3><?php echo fibonacci($_POST["posicao"]); ?></h3>
-            
+            <h2>Resultado:</h2>
+            <div class="resultado">
+                <?php
+                if (isset($_POST["posicao"])) {
+                ?>
 
-            <?php
-            }
-            ?>
-        </div>
-    </form>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+                    <h3><?php echo numero_primo($_POST["posicao"]); ?></h3>
+
+
+                <?php
+                }
+                ?>
+            </div>
+        </form>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </div>
 </body>
 
 </html>
